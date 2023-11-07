2f81dedd74b2e76d22df54a7d37a0fe03770fa95
Александр Угаров <sasha.ugarov.02@mail.ru>
2023-11-07 19:40:59 +0300

    rules and some task

--- ch1.ts
+++ ch1.ts
@@ -8,10 +8,28 @@ const outer = () => {
 		console.log( a + b );	// 3
 	}
 
-	inner();
+    inner();
 
 	// здесь у нас есть доступ только к  `a`
 	console.log( a );			// 1
 }
 
-outer();
+outer()
+
+const SPENDING_THRESHOLD = 200;
+const TAX_RATE = 0.08;
+const PHONE_PRICE = 99.99;
+const ACCESSORY_PRICE = 9.99;
+
+const x = (arg1: number, arg2: number, arg3: number) => {
+    let cost = 0;
+    while (cost < arg1){
+        let y = arg1
+        cost += y - (arg2*TAX_RATE + arg2 + arg3*TAX_RATE + arg3)
+        y-= (arg2*TAX_RATE + arg2 + arg3*TAX_RATE + arg3)
+    }
+    console.log(cost)
+}
+
+
+x(SPENDING_THRESHOLD, PHONE_PRICE, ACCESSORY_PRICE)
